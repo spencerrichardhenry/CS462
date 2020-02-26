1e1528641de7713f3d845802ad9ee9c0ac1ff54e
Spencer Henry <spencerrichardhenry@gmail.com>
2020-02-25 21:15:09 -0700

    lab5 final

--- lab5/src/store/index.js
+++ lab5/src/store/index.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
     temps: null,
     violations: null,
     tempThreshold: 73.0,
-    profile: null
+    profile: { location: "", name: "", receiving_phone: "", sending_phone: "", threshold: "" }
   },
   mutations: {
     setTemps(state, temps) {
@@ -25,7 +25,22 @@ export default new Vuex.Store({
     setProfile(state, profile) {
       state.profile = profile;
       state.tempThreshold = profile.threshold;
-    }
+    },
+    setLocation(state, location) {
+      state.profile.location = location;
+    },
+    setName(state, name) {
+      state.profile.name = name;
+    },
+    setReceiving_phone(state, receiving_phone) {
+      state.profile.receiving_phone = receiving_phone;
+    },
+    setSending_phone(state, sending_phone) {
+      state.profile.sending_phone = sending_phone;
+    },
+    setThreshold(state, threshold) {
+      state.profile.threshold = threshold;
+    },
   },
   actions: {
     async getViolations(context) {
@@ -42,6 +57,7 @@ export default new Vuex.Store({
         let response = await axios.get("/sky/cloud/GM9rGepSqHudoUUo4DXifS/temperature_store/temperatures");
         context.commit("setTemps", response.data);
         await this.dispatch("getViolations", context);
+        await this.dispatch("getProfile", context);
         return "";
       } catch (error) {
         return "";
@@ -55,7 +71,34 @@ export default new Vuex.Store({
         console.log(error);
         return "";
       }
-    }
+    },
+    async updateProfile(context, url) {
+      try {
+        console.log(url)
+        let response = await axios.post(url);
+        // let response = await axios.post("/sky/event/GM9rGepSqHudoUUo4DXifS/123/sensor/profile_updated?location=" + location + "&name=" + name + "&receiving=" + receiving + "&sending=" + sending + "&threshold=" + threshold);
+        console.log("response from updateProfile api: " + response);
+        console.log("context: " + context);
+      } catch (error) {
+        console.log(error);
+        return "";
+      }
+    },
+    locationSetter(context, location) {
+      context.commit("setLocation", location);
+    },
+    nameSetter(context, name) {
+      context.commit("setName", name);
+    },
+    receiving_phoneSetter(context, receiving_phone) {
+      context.commit("setReceiving_phone", receiving_phone);
+    },
+    sending_phoneSetter(context, sending_phone) {
+      context.commit("setSending_phone", sending_phone);
+    },
+    thresholdSetter(context, threshold) {
+      context.commit("setThreshold", threshold);
+    },
   },
   modules: {}
 });
--- lab5/src/views/Profile.vue
+++ lab5/src/views/Profile.vue
@@ -1,7 +1,16 @@
 <template>
-  <div v-if="location !== null">
-    <p>yo</p>
-    <input v-model="receiving_phone" type="text" />
+  <div>
+    <div v-if="location">
+      <a>
+        <router-link to="/">Back</router-link>
+      </a>
+      <input v-model="location" type="text" />
+      <input v-model="name" type="text" />
+      <input v-model="receiving_phone" type="text" />
+      <input v-model="sending_phone" type="text" />
+      <input v-model="threshold" type="text" />
+    </div>
+    <button v-on:click="updateProfile">Update Values</button>
   </div>
 </template>
 
@@ -9,7 +18,13 @@
 export default {
   name: "Profile",
   data() {
-    return {};
+    return {
+      test1: "",
+      test2: "",
+      test3: "",
+      test4: "",
+      test5: ""
+    };
   },
   created() {
     this.$store.dispatch("getProfile");
@@ -23,21 +38,85 @@ export default {
   //   return {"location": sensor_location, "name": sensor_name, "threshold": temperature_threshold, "receiving_phone": receiving_phone, "sending_phone": sending_phone }
   // }
   computed: {
-    location() {
-      return this.$store.state.profile.location;
+    location: {
+      get: function() {
+        return this.$store.state.profile.location;
+      },
+      set: function(newLocation) {
+        this.$store.dispatch("locationSetter", newLocation);
+      }
     },
-    name() {
-      return this.$store.state.profile.name;
+    name: {
+      get: function() {
+        return this.$store.state.profile.name;
+      },
+      set: function(newName) {
+        this.$store.dispatch("nameSetter", newName);
+      }
     },
-    receiving_phone() {
-      return this.$store.state.profile.receiving_phone;
+    receiving_phone: {
+      get: function() {
+        return this.$store.state.profile.receiving_phone;
+      },
+      set: function(newReceiving_phone) {
+        this.$store.dispatch("receiving_phoneSetter", newReceiving_phone);
+      }
     },
-    sending_phone() {
-      return this.$store.state.profile.sending_phone;
+    sending_phone: {
+      get: function() {
+        return this.$store.state.profile.sending_phone;
+      },
+      set: function(newSending_phone) {
+        this.$store.dispatch("sending_phoneSetter", newSending_phone);
+      }
     },
-    threshold() {
-      return this.$store.state.profile.threshold;
+    threshold: {
+      get: function() {
+        return this.$store.state.profile.threshold;
+      },
+      set: function(newThreshold) {
+        this.$store.dispatch("thresholdSetter", newThreshold);
+      }
+    }
+    // name() {
+    //   return this.$store.state.profile.name;
+    // },
+    // receiving_phone() {
+    //   return this.$store.state.profile.receiving_phone;
+    // },
+    // sending_phone() {
+    //   return this.$store.state.profile.sending_phone;
+    // },
+    // threshold() {
+    //   return this.$store.state.profile.threshold;
+    // }
+  },
+  methods: {
+    async updateProfile() {
+      console.log(this.name);
+      var url =
+        "/sky/event/GM9rGepSqHudoUUo4DXifS/123/sensor/profile_updated?location=" +
+        this.location +
+        "&name=" +
+        this.name +
+        "&receiving=" +
+        this.receiving_phone +
+        "&sending=" +
+        this.sending_phone +
+        "&threshold=" +
+        this.threshold;
+      try {
+        this.$store.dispatch("updateProfile", url);
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 };
 </script>
+
+<style scoped>
+input {
+  margin-bottom: 1vw;
+}
+</style>
